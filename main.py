c65da11d8069551487ddf986bd1e1e345e571d71
DungNV <nguyenvandung30271@gmail.com>
2025-11-23 01:23:09 +0700

    dat

--- main.py
+++ main.py
@@ -24,3 +24,7 @@ def delete_item(item_id: int):
 @app.put("/Dung/{item_id}")
 def update_item(item_id: int, item: dict):
     return {"item_id": item_id, "item": item}
+
+@app.put("/dat/{item_id}")
+def update_item(item_id: int, item: dict):
+    return {"item_id": item_id, "item": item}
